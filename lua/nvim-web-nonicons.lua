bcc0c9ae732de3f41e01dd385aa276055fe8c624
yamatsum <sy12250@gmail.com>
2021-03-12 03:29:40 +0900

    feat: add icons setting.

--- lua/nvim-web-nonicons.lua
+++ lua/nvim-web-nonicons.lua
@@ -1,9 +1,25 @@
 require "nvim-web-devicons".setup {
   override = {
+    -- File
     ["default_icon"] = {
       icon = "",
       color = "#6d8086",
       name = "Default"
+    },
+    ["folder"] = {
+      icon = "",
+      color = "#6d8086",
+      name = "Folder"
+    },
+    ["folder-open"] = {
+      icon = "",
+      color = "#6d8086",
+      name = "FolderOpen"
+    },
+    ["folder-link"] = {
+      icon = "",
+      color = "#6d8086",
+      name = "FolderLink"
     },
     [".gitignore"] = {
       icon = "",
@@ -159,6 +175,125 @@ require "nvim-web-devicons".setup {
       icon = "",
       color = "#701516",
       name = "Rb"
+    },
+    -- Mode
+    ["normal"] = {
+      icon = "",
+      name = "Normal"
+    },
+    ["insert"] = {
+      icon = "",
+      name = "Insert"
+    },
+    ["visual"] = {
+      icon = "",
+      name = "Visual"
+    },
+    ["command"] = {
+      icon = "",
+      name = "Command"
+    },
+    ["replace"] = {
+      icon = "",
+      name = "Replace"
+    },
+    -- Mode
+    ["function"] = {
+      icon = "",
+      name = "Function"
+    },
+    ["variable"] = {
+      icon = "",
+      name = "Variable"
+    },
+    ["constant"] = {
+      icon = "",
+      name = "Constant"
+    },
+    ["struct"] = {
+      icon = "",
+      name = "Struct"
+    },
+    ["class"] = {
+      icon = "",
+      name = "Class"
+    },
+    ["interface"] = {
+      icon = "",
+      name = "Interface"
+    },
+    ["text"] = {
+      icon = "",
+      name = "Text"
+    },
+    ["enum"] = {
+      icon = "",
+      name = "Enum"
+    },
+    ["color"] = {
+      icon = "",
+      name = "Color"
+    },
+    ["property"] = {
+      icon = "",
+      name = "Property"
+    },
+    ["field"] = {
+      icon = "",
+      name = "Field"
+    },
+    ["unit"] = {
+      icon = "",
+      name = "Unit"
+    },
+    ["event"] = {
+      icon = "",
+      name = "Event"
+    },
+    ["snipet"] = {
+      icon = "",
+      name = "Snipet"
+    },
+    ["operator"] = {
+      icon = "",
+      name = "Operator"
+    },
+    ["reference"] = {
+      icon = "",
+      name = "Reference"
+    },
+    ["type"] = {
+      icon = "",
+      name = "Type"
+    },
+    -- other
+    ["lock"] = {
+      icon = "",
+      name = "Lock"
+    },
+    ["branch"] = {
+      icon = "",
+      name = "Branch"
+    },
+    ["telescope"] = {
+      icon = "",
+      name = "Telescope"
+    },
+    ["working"] = {
+      icon = "",
+      name = "Working"
+    },
+    ["error"] = {
+      icon = "",
+      name = "Error"
+    },
+    ["check"] = {
+      icon = "",
+      name = "Check"
+    },
+    ["moved"] = {
+      icon = "",
+      name = "Moved"
     }
   },
   default = true
