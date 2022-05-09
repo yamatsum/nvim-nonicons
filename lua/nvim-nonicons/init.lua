25cd7e55402ff9f1f9ef4bcd4b64548a39041ae6
yamatsum <sy12250@gmail.com>
2022-05-09 12:15:34 +0900

    feat!: setup by pure lua

---
+++ lua/nvim-nonicons/init.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+local icons = require("nvim-nonicons.mapping")
+
+local DEFAULT_OPTIONS = {
+    override_devicons = true
+}
+
+function M.setup(options)
+    M.options = vim.tbl_deep_extend("force", DEFAULT_OPTIONS, options or {})
+
+    if (M.options.override_devicons ~= false) then
+        require("nvim-web-devicons.override")
+    end
+end
+
+function M.get(name)
+    return vim.fn.nr2char(icons[name])
+end
+
+M.options = nil
+
+return M
